24175f782244ae461f2d2d32d9f5838b1f1566cd
aviv187 <avivb35@gmail.com>
2021-04-18 23:01:06 +0300

    make all the setting to one function

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,59 @@ import "./App.css";
 
 import BarChart from "./BarChart";
 
+interface SettingsPropery {
+  valuesArrLength: number;
+  containerHeight: number;
+  containerWidth: number;
+  barGap: number;
+  maxValue: number;
+}
+
 const App: React.FC = () => {
   const [values, setValues] = useState(['10', '20', '30']);
-  const [valuesArrLength, setValuesArrLength] = useState(values.length);
-  const [maxValue, setMaxValue] = useState(30);
-  const [containerHeight, setContainerHeight] = useState(400);
-  const [containerWidth, setContainerWidth] = useState(500);
-  const [barGap, setBarGap] = useState(5);
+  const [settings, setSettings] = useState<SettingsPropery>({
+    valuesArrLength: values.length,
+    containerHeight: 400,
+    containerWidth: 500,
+    barGap: 5,
+    maxValue: 30,
+  });
+
+  const _updateSettings = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let _settings = { ...settings };
+
+    const key = e.target.getAttribute('data-type');
+    if (key !== null && key in _settings) {
+      _settings[key] = +e.target.value;
+    }
+
+    switch (e.target.getAttribute('data-type')) {
+      case 'containerHeight':
+        _settings['containerHeight'] = +e.target.value;
+        break;
+      case 'containerWidth':
+        _settings['containerWidth'] = +e.target.value;
+        break;
+      case 'barGap':
+        _settings['barGap'] = +e.target.value;
+        break;
+      case 'maxValue':
+        _settings['maxValue'] = +e.target.value;
+        break;
+      case 'valuesArrLength':
+        _settings['valuesArrLength'] = +e.target.value;
+        break;
+      default:
+        break;
+    }
+
+    setSettings(_settings);
+  }
+
 
   const _updateValues = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValues(e.target.value.split(','));
   };
-
-  const _updateValuesArrLength = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValuesArrLength(+e.target.value);
-  };
-
-  const _updateMaxValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMaxValue(+e.target.value);
-  };
-
-  const _updateContainerHeight = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setContainerHeight(+e.target.value);
-  };
-
-  const _updateContainerWidth = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setContainerWidth(+e.target.value);
-  };
-
-  const _updateBarGap = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBarGap(+e.target.value);
-  };
-
-  useEffect(() => {
-    setValuesArrLength(values.length);
-  }, [values]);
 
   const _changeValueArrey = () => {
     let newValuesArr: string[];
@@ -49,13 +67,13 @@ const App: React.FC = () => {
       newValuesArr = [...values];
     }
 
-    const dif = Math.abs(values.length - valuesArrLength);
+    const dif = Math.abs(values.length - settings.valuesArrLength);
 
-    if (values.length > valuesArrLength) {
+    if (values.length > settings.valuesArrLength) {
       for (let i = 0; i < dif; i++) {
         newValuesArr.pop();
       }
-    } else if (values.length < valuesArrLength) {
+    } else if (values.length < settings.valuesArrLength) {
       for (let i = 0; i < dif; i++) {
         newValuesArr.push(createRamdonNum());
       }
@@ -65,28 +83,37 @@ const App: React.FC = () => {
   };
 
   const createRamdonNum = () => {
-    const random = Math.floor((Math.random() * (maxValue + 1)));
+    const random = Math.floor((Math.random() * (settings.maxValue + 1)));
     return random.toString()
   }
+
+  useEffect(() => {
+    setSettings(state => {
+      let _settings = { ...state };
+      _settings['valuesArrLength'] = values.length;
+
+      return _settings;
+    });
+  }, [values]);
 
   return (
     <div className="App">
       <div className="values">
         Enter the container height:
-        <input type="number" value={containerHeight} onChange={_updateContainerHeight} />
+        <input type="number" data-type='containerHeight' value={settings.containerHeight} onChange={_updateSettings} />
         Enter the container width:
-        <input type="number" value={containerWidth} onChange={_updateContainerWidth} />
+        <input type="number" data-type='containerWidth' value={settings.containerWidth} onChange={_updateSettings} />
         Enter the bar gap:
-        <input type="number" value={barGap} onChange={_updateBarGap} />
+        <input type="number" data-type='barGap' value={settings.barGap} onChange={_updateSettings} />
         Enter values separated by comma:
         <input type="text" value={values} onChange={_updateValues} />
           Enter the number of values:
-        <input type="number" value={valuesArrLength} onChange={_updateValuesArrLength} />
+        <input type="number" data-type='valuesArrLength' value={settings.valuesArrLength} onChange={_updateSettings} />
         Enter the max value:
-        <input type="number" value={maxValue} onChange={_updateMaxValue} />
+        <input type="number" data-type='maxValue' value={settings.maxValue} onChange={_updateSettings} />
         <button onClick={_changeValueArrey}>add/delete values</button>
       </div>
-      <BarChart values={values} containerHeight={containerHeight} containerWidth={containerWidth} barGap={barGap} />
+      <BarChart values={values} containerHeight={settings.containerHeight} containerWidth={settings.containerWidth} barGap={settings.barGap} />
     </div>
   );
 }
